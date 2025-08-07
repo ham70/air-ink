7fc841da09256e672061469b5636eedd6efd9616
Matthew Ham <hammatthew70@gmail.com>
2025-08-07 16:51:33 -0400

    built new painter class

---
+++ painter.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+import os
+
+class Painter():
+  def __init__(self):
+    self.imgCanvas = np.zeros((720, 1280, 3), np.uint8)
+
+  #methods ==================================================
+  def paint(self, prev, curr, color, thickness):
+    cv2.line(self.imgCanvas, prev, curr, color, thickness)
+
+  def getCanvas(self):
+    return self.imgCanvas
+  
+  def getInvCanvas(self):
+    imgGray = cv2.cvtColor(self.imgCanvas, cv2.COLOR_BGR2GRAY)
+    _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
+    imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
+
+    return imgInv
